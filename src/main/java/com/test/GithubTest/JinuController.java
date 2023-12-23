fbe92987cef4043d3d12274a1ca6aac0386085b6
Jinu Yi <dionisos198@naver.com>
2023-12-23 21:45:12 +0900

    feature/test-Local

--- src/main/java/com/test/GithubTest/JinuController.java
+++ src/main/java/com/test/GithubTest/JinuController.java
@@ -8,4 +8,7 @@ public class JinuController {
         System.out.println("A 기능 구현 성공!!! pull request하구 merge 기다리는 중 !! ㅎㅎ");
         System.out.println(" 코드를 갈아 엎었다");
     }
+    public void testLocal(){
+        System.out.println("local 브랜치 옮길때 어떻게 되는가 테스트");
+    }
 }
